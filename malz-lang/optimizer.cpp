5ce22e42fec199921a1ee267de39fa02c93fdb65
Yassir Karroum <ukarroum17@gmail.com>
2024-04-06 17:05:30 +0200

    [optimizer] Improved dead code pass to handle more use cases

--- malz-lang/optimizer.cpp
+++ malz-lang/optimizer.cpp
@@ -1,5 +1,6 @@
 #include <unordered_set>
 #include <string>
+#include <ranges>
 
 #include "optimizer.hpp"
 
@@ -19,9 +20,24 @@ Block deadCodeOpt(const Block& block)
             for(auto& arg: instr.args)
                 used.insert(arg);
 
-        for(auto& instr: block)
-            if(!(!instr.dest.empty() && !used.contains(instr.dest)))
-                optimized_block.push_back(instr);
+        for(int i = 0; i < block.size(); i++)
+            if(!(!block[i].dest.empty() && !used.contains(block[i].dest)))
+            {
+                // Check for definitions without usage or multiple definitions were the previous ones have no effect
+                bool useless_affect = true;
+                for(int j = i + 1; j < block.size(); j++)
+                {
+                    if(std::ranges::find(block[j].args, block[i].dest) != block[j].args.end())
+                    {
+                        useless_affect = false;
+                        break;
+                    }
+                    if(block[j].dest == block[i].dest)
+                        break;
+                }
+                if(!useless_affect)
+                    optimized_block.push_back(block[i]);
+            }
     }while(optimized_block != original_block);
 
     return optimized_block;
