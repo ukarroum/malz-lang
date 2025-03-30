8b12afc3bc8ce3967dead6934b85d772318b620f
Yassir Karroum <ukarroum17@gmail.com>
2025-03-30 10:48:10 +0200

    Added debug crate

--- malz-lang/src/chunk.rs
+++ malz-lang/src/chunk.rs
@@ -1,12 +1,22 @@
-use std::Vec;
+use std::vec;
 
 pub mod chunk {
+    #[repr(u8)]
     pub enum OpCode {
-        RETURN
+        RETURN = 1
     }
 
     pub struct Chunk {
-        code: Vec<u8>
+        pub code: Vec<u8>
+    }
+
+    impl Chunk {
+        pub fn new() -> Self {
+            Self { code: Vec::new() }
+        }
+        pub fn write(&mut self, byte: u8){
+            self.code.push(byte);
+        }
     }
 }
 
--- malz-lang/src/main.rs
+++ malz-lang/src/main.rs
@@ -1,7 +1,14 @@
 mod chunk;
+mod debug;
 
+pub use crate::chunk::chunk::Chunk;
 pub use crate::chunk::chunk::OpCode;
+pub use crate::debug::debug::disChunk;
 
 fn main() {
-    println!("Hello, world!");
+    let mut code_chunk = Chunk::new();
+
+    code_chunk.write(OpCode::RETURN as u8);
+
+    disChunk(&chunk, "test chunk");
 }
