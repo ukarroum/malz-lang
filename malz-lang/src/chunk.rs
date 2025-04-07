f13e52a87620654a93e417cc9a933940ea795e6b
Yassir Karroum <ukarroum17@gmail.com>
2025-04-07 19:11:00 +0200

    Add CONSTANT

--- malz-lang/src/chunk.rs
+++ malz-lang/src/chunk.rs
@@ -1,18 +1,27 @@
+use crate::value::Value;
+
 #[repr(u8)]
 pub enum OpCode {
-    RETURN = 0
+    RETURN = 0,
+    CONSTANT = 1
 }
 
 pub struct Chunk {
-    pub code: Vec<u8>
+    pub code: Vec<u8>,
+    pub constants: Vec<Value>
 }
 
 impl Chunk {
     pub fn new() -> Self {
-        Self { code: Vec::new() }
+        Self { code: Vec::new(), constants: Vec::new() }
     }
     pub fn write(&mut self, byte: u8){
         self.code.push(byte);
+    }
+    pub fn add_constant(&mut self, val Value) -> usize {
+        self.constants.push(val);
+
+        self.constants.len() - 1
     }
 }
 impl TryFrom<u8> for OpCode {
@@ -21,6 +30,7 @@ impl TryFrom<u8> for OpCode {
     fn try_from(value: u8) -> Result<Self, Self::Error>{
         match value {
             0 => Ok(OpCode::RETURN),
+            1 => Ok(OpCode::CONSTANT),
             _ => Err("Invalid opcode")
         }
     }
--- malz-lang/src/main.rs
+++ malz-lang/src/main.rs
@@ -1,5 +1,6 @@
 mod chunk;
 mod debug;
+mod value;
 
 use crate::chunk::Chunk;
 use crate::chunk::OpCode;
@@ -7,6 +8,10 @@ use crate::debug::dis_chunk;
 
 fn main() {
     let mut code_chunk = Chunk::new();
+
+    let const = code_chunk.add_constant(1.2);
+    code_chunk.write(OpCode::CONSTANT);
+    code_chunk.write(const);
 
     code_chunk.write(OpCode::RETURN as u8);
 
