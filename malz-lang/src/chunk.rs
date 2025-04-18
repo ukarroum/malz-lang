0055db40233fb4b4ce6a673200698c55b1f5b04f
Yassir Karroum <ukarroum17@gmail.com>
2025-04-18 08:47:04 +0200

    Add run-length encoding for line numbers

--- malz-lang/src/chunk.rs
+++ malz-lang/src/chunk.rs
@@ -9,7 +9,7 @@ pub enum OpCode {
 pub struct Chunk {
     pub code: Vec<u8>,
     pub constants: Vec<Value>,
-    pub lines: Vec<i32>
+    pub lines: Vec<(i32, usize)>
 }
 
 impl Chunk {
@@ -18,12 +18,44 @@ impl Chunk {
     }
     pub fn write(&mut self, byte: u8, line: i32){
         self.code.push(byte);
-        self.lines.push(line)
+
+        match self.lines.last_mut() {
+            None => self.lines.push((line, 1)),
+            Some((o_line, _nb_offset)) => if *o_line == line {
+                self.lines.last_mut().unwrap().1 += 1;
+            }
+            else {
+                self.lines.push((line, 1));
+            }
+        }
     }
     pub fn add_constant(&mut self, val: Value) -> usize {
         self.constants.push(val);
-
         self.constants.len() - 1
+    }
+
+    pub fn get_line(&self, offset: usize) -> i32 {
+        let mut idx: usize = 0;
+
+        let mut offset = offset;
+
+        while offset != 0 {
+            if offset <= self.lines[idx].1 {
+                return self.lines[idx].0;
+            }
+            else {
+                offset -= self.lines[idx].1;
+            }
+
+            idx += 1;
+        }
+
+        if offset == 0 {
+            return self.lines[idx].0;
+        }
+        else {
+            panic!("lines and offset mismatch");
+        }
     }
 }
 impl TryFrom<u8> for OpCode {
